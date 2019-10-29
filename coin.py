46653679539c3c667f0678dee007ff7a2beeb95b
shigekis <45548424+shigekis@users.noreply.github.com>
2019-10-29 15:38:22 +0900

    Update coin.py

--- coin.py
+++ coin.py
@@ -2,11 +2,32 @@
 
 import random
 
-print("coin-toss")
-face=random.randint(0,1)
+def main():
 
-if face==0 :
-  print("Head.")
-else:
-  print("Tail.")
+  print("coin-toss")
+  face=random.randint(0,1)
 
+  if face==0 :
+    return "Head"
+  else:
+    return "Tail"
+
+    
+print(main());
+
+def testmain():
+  head=0
+  tail=0
+  # HeadかTainのいずれかが実行されることを確認します。
+  for i in 0..9999:
+    result=main();
+    if result == "Head":
+      head++
+    else if result == "Tail":
+      tail++
+    else:
+      assert false
+  assert  head < 5100
+  assert  tail < 5100
+
+  
